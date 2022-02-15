730fd9dc33a9796309a5f4bd3f6953161c6edd10
EsAlco <90135660+EsAlco@users.noreply.github.com>
2022-02-15 16:36:17 +0100

    Initial Commit

---
+++ P1-ExpandableBottomSheet/P1_ExpandableBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  P1_ExpandableBottomSheetApp.swift
+//  P1-ExpandableBottomSheet
+//
+//  Created by Esther Alcoceba Gutiérrez de León on 15/2/22.
+//
+
+import SwiftUI
+
+@main
+struct P1_ExpandableBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
